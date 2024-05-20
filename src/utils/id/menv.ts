938767dbc71bdc5c7e3317b9ebe1a9689d5ebdce
Il Harper <hi@ilharper.com>
2024-05-20 17:51:27 +0800

    refactor(id): menv

--- src/utils/id/menv.ts
+++ src/utils/id/menv.ts
@@ -1,5 +1,5 @@
 import type { Context } from 'koishi'
 
-// Currently empty string
+// Use L1 to reset mid
 // eslint-disable-next-line @typescript-eslint/require-await
-export const getMachineEnv = async (_ctx: Context): Promise<string> => ''
+export const getMachineEnv = async (_ctx: Context): Promise<string> => '\n'
